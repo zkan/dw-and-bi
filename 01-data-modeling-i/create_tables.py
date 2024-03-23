aaadbe60639e5978170213f1cadb4e3d808853e2
Kan Ouivirach <kan@odds.team>
2024-03-23 09:02:03 +0000

    Finish up to project 04

---
+++ 01-data-modeling-i/create_tables.py
@@ -0,0 +1,78 @@
+from typing import NewType
+
+import psycopg2
+
+
+PostgresCursor = NewType("PostgresCursor", psycopg2.extensions.cursor)
+PostgresConn = NewType("PostgresConn", psycopg2.extensions.connection)
+
+table_drop_events = "DROP TABLE IF EXISTS events"
+table_drop_actors = "DROP TABLE IF EXISTS actors"
+
+table_create_actors = """
+    CREATE TABLE IF NOT EXISTS actors (
+        id int,
+        login text,
+        PRIMARY KEY(id)
+    )
+"""
+table_create_events = """
+    CREATE TABLE IF NOT EXISTS events (
+        id text,
+        type text,
+        actor_id int,
+        PRIMARY KEY(id),
+        CONSTRAINT fk_actor FOREIGN KEY(actor_id) REFERENCES actors(id)
+    )
+"""
+
+create_table_queries = [
+    table_create_actors,
+    table_create_events,
+]
+drop_table_queries = [
+    table_drop_events,
+    table_drop_actors,
+]
+
+
+def drop_tables(cur: PostgresCursor, conn: PostgresConn) -> None:
+    """
+    Drops each table using the queries in `drop_table_queries` list.
+    """
+    for query in drop_table_queries:
+        cur.execute(query)
+        conn.commit()
+
+
+def create_tables(cur: PostgresCursor, conn: PostgresConn) -> None:
+    """
+    Creates each table using the queries in `create_table_queries` list.
+    """
+    for query in create_table_queries:
+        cur.execute(query)
+        conn.commit()
+
+
+def main():
+    """
+    - Drops (if exists) and Creates the sparkify database.
+    - Establishes connection with the sparkify database and gets
+    cursor to it.
+    - Drops all the tables.
+    - Creates all tables needed.
+    - Finally, closes the connection.
+    """
+    conn = psycopg2.connect(
+        "host=127.0.0.1 dbname=postgres user=postgres password=postgres"
+    )
+    cur = conn.cursor()
+
+    drop_tables(cur, conn)
+    create_tables(cur, conn)
+
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
---
+++ 01-data-modeling-i/etl.py
@@ -0,0 +1,97 @@
+import glob
+import json
+import os
+from typing import List
+
+import psycopg2
+
+
+def get_files(filepath: str) -> List[str]:
+    """
+    Description: This function is responsible for listing the files in a directory
+    """
+
+    all_files = []
+    for root, dirs, files in os.walk(filepath):
+        files = glob.glob(os.path.join(root, "*.json"))
+        for f in files:
+            all_files.append(os.path.abspath(f))
+
+    num_files = len(all_files)
+    print(f"{num_files} files found in {filepath}")
+
+    return all_files
+
+
+def process(cur, conn, filepath):
+    # Get list of files from filepath
+    all_files = get_files(filepath)
+    print(all_files)
+
+    for datafile in all_files:
+        with open(datafile, "r") as f:
+            data = json.loads(f.read())
+            for each in data:
+                # Print some sample data
+                
+                if each["type"] == "IssueCommentEvent":
+                    print(
+                        each["id"], 
+                        each["type"],
+                        each["actor"]["id"],
+                        each["actor"]["login"],
+                        each["repo"]["id"],
+                        each["repo"]["name"],
+                        each["created_at"],
+                        each["payload"]["issue"]["url"],
+                    )
+                else:
+                    print(
+                        each["id"], 
+                        each["type"],
+                        each["actor"]["id"],
+                        each["actor"]["login"],
+                        each["repo"]["id"],
+                        each["repo"]["name"],
+                        each["created_at"],
+                    )
+
+                # Insert data into tables here
+                insert_statement = f"""
+                    INSERT INTO actors (
+                        id,
+                        login
+                    ) VALUES ({each["actor"]["id"]}, '{each["actor"]["login"]}')
+                    ON CONFLICT (id) DO NOTHING
+                """
+                # print(insert_statement)
+                cur.execute(insert_statement)
+
+                # Insert data into tables here
+                insert_statement = f"""
+                    INSERT INTO events (
+                        id,
+                        type,
+                        actor_id
+                    ) VALUES ('{each["id"]}', '{each["type"]}', '{each["actor"]["id"]}')
+                    ON CONFLICT (id) DO NOTHING
+                """
+                # print(insert_statement)
+                cur.execute(insert_statement)
+
+                conn.commit()
+
+
+def main():
+    conn = psycopg2.connect(
+        "host=127.0.0.1 dbname=postgres user=postgres password=postgres"
+    )
+    cur = conn.cursor()
+
+    process(cur, conn, filepath="../data")
+
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
